1400961c8040c4dd605041added317332f226294
Matthew <kohney319m@gmail.com>
2021-09-30 11:46:52 -0600

    Add styling and add priority dropdown functionality

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 document.addEventListener("DOMContentLoaded", (e) => {
-  // definitions
+  // DEFINITIONS
   const tasks = document.querySelector('#tasks');
   const taskForm = document.querySelector('#create-task-form');
   const inputField = document.querySelector('#new-task-description');
+  
 
-  // handle submit
-  taskForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    addListItem(inputField.value);
-    taskForm.reset();
-  })
-
+  // FUNCTIONS
   // add list item
   const addListItem = (input) => {
+    if (input.length === 0) {
+      alert("Enter a task description to add tasks!");
+      return;
+    }
     // create list item and add it to DOM
     const li = document.createElement('li');
+    li.className = 'list-item';
+    li.style.borderColor = importanceColor();
     li.textContent = `${input} `;
     tasks.appendChild(li);
 
@@ -25,6 +26,30 @@ document.addEventListener("DOMContentLoaded", (e) => {
     li.appendChild(btn);
   }
   
+  // add dropdown functionality
+  const importanceColor = () => {
+    let color;
+    const importance = document.querySelector('#importanceDropdown').value;
+    if (importance === 'urgent') {
+      color = '#F51C16';
+    } else if (importance === 'important') {
+      color = '#F5A62F';
+    } else if (importance === 'no-rush') {
+      color = '#3BF578';
+    }
+    return color;
+  }
+  console.log(document.querySelector('#importanceDropdown').value);
+
+
+  // EVENT HANDLERS
+  // handle submit
+    taskForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      addListItem(inputField.value);
+      taskForm.reset();
+    })
+
   // delete if list item button is clicked
   document.addEventListener('click', (e) => {
     if (e.target.className === 'delete') {
